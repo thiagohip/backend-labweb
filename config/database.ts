35d4edf40696f964ce86957b76dfb4f3dc23896b
thiagohip <thiaggohipolitomoreira@gmail.com>
2023-09-29 10:04:26 -0300

    predeploy

--- config/database.ts
+++ config/database.ts
@@ -41,9 +41,7 @@ const databaseConfig: DatabaseConfig = {
         user: Env.get('MYSQL_USER'),
         password: Env.get('MYSQL_PASSWORD', ''),
         database: Env.get('MYSQL_DB_NAME'),
-        ssl: {
-          rejectUnauthorized: false
-        }
+        
       },
       migrations: {
         naturalSort: true,
@@ -71,6 +69,9 @@ const databaseConfig: DatabaseConfig = {
         user: Env.get('PG_USER'),
         password: Env.get('PG_PASSWORD', ''),
         database: Env.get('PG_DB_NAME'),
+        ssl: {
+          rejectUnauthorized: false
+        }
       },
       migrations: {
         naturalSort: true,
